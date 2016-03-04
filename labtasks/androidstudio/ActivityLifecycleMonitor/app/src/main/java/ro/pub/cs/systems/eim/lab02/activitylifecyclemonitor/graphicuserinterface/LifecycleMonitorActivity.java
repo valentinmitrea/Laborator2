29d7cf1403627c1c2127b4497c2cfea4bea03e43
Student Elemente de Informatica Mobila 2016 <student@eim2016>
2016-03-04 06:27:55 +0200

    initial commit

---
+++ labtasks/androidstudio/ActivityLifecycleMonitor/app/src/main/java/ro/pub/cs/systems/eim/lab02/activitylifecyclemonitor/graphicuserinterface/LifecycleMonitorActivity.java
@@ -0,0 +1,93 @@
+package ro.pub.cs.systems.eim.lab02.activitylifecyclemonitor.graphicuserinterface;
+
+import android.app.ActionBar.LayoutParams;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import ro.pub.cs.systems.eim.lab02.activitylifecyclemonitor.R;
+import ro.pub.cs.systems.eim.lab02.activitylifecyclemonitor.general.Constants;
+import ro.pub.cs.systems.eim.lab02.activitylifecyclemonitor.general.Utilities;
+
+public class LifecycleMonitorActivity extends Activity {
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        @SuppressLint("InflateParams")
+        public void onClick(View view) {
+            EditText usernameEditText = (EditText)findViewById(R.id.username_edit_text);
+            EditText passwordEditText = (EditText)findViewById(R.id.password_edit_text);
+
+            if(((Button)view).getText().toString().equals(getResources().getString(R.string.ok_button_content))) {
+                LayoutInflater layoutInflater = (LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                View popupContent = null;
+                if (Utilities.allowAccess(getApplicationContext(), username, password)) {
+                    popupContent = layoutInflater.inflate(R.layout.popup_window_authentication_success, null);
+                } else {
+                    popupContent = layoutInflater.inflate(R.layout.popup_window_authentication_fail, null);
+                }
+                final PopupWindow popupWindow = new PopupWindow(popupContent, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                Button dismissButton = (Button)popupContent.findViewById(R.id.dismiss_button);
+                dismissButton.setOnClickListener(new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        popupWindow.dismiss();
+                    }
+                });
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+            }
+
+            if(((Button)view).getText().toString().equals(getResources().getString(R.string.cancel_button_content))) {
+                usernameEditText.setText(getResources().getString(R.string.empty));
+                passwordEditText.setText(getResources().getString(R.string.empty));
+            }
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lifecycle_monitor);
+
+        Button okButton = (Button)findViewById(R.id.ok_button);
+        okButton.setOnClickListener(buttonClickListener);
+        Button cancelButton = (Button)findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(buttonClickListener);
+
+        Log.d(Constants.TAG, "onCreate() method was invoked");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_lifecycle_monitor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
